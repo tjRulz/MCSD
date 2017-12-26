4da88463b897b89d78e9b090b2278fe5c8fe1f55
Tejas Thaker <Tejas.Thaker@morningstar.com>
2017-12-26 11:42:56 -0600

    initial commit

---
+++ 70-483/70-483/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _70_483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
